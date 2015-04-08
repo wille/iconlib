7c496a24f88697f728f04b2e00d61e44c4193bc9
redpois0n <red@cmail.nu>
2015-04-09 01:00:29 +0200

    basic icon methods added

--- src/iconlib/IconUtils.java
+++ src/iconlib/IconUtils.java
@@ -1,5 +1,48 @@
 package iconlib;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 public class IconUtils {
+	
+	/**
+	 * Cache of icons
+	 */
+	private static final Map<String, Icon> CACHE = new HashMap<String, Icon>();
+
+	/**
+	 * Gets icon from default path (/icons/name.png)
+	 * @param path
+	 * @return the icon
+	 */
+	public static Icon getIcon(String path) {
+		return getIcon(path, true);
+	}
+	
+	/**
+	 * Returns icon from path (
+	 * @param path
+	 * @param defaultPath If should return icon with default extension and in default path
+	 * @return the icon
+	 */
+	public static Icon getIcon(String path, boolean defaultPath) {
+		if (defaultPath) {
+			path = "/icons/" + path + ".png";
+		}
+		
+		Icon icon;
+		
+		if (CACHE.containsKey(path)) {
+			icon = CACHE.get(path);
+		} else {
+			icon = new ImageIcon(IconUtils.class.getResource(path));
+			CACHE.put(path, icon);
+		}
+		
+		return icon;
+	}
 
 }
